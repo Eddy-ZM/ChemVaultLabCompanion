0abf8a0dd216de3239289f453166a8df77f29b02
ziwenmu <ziwen.mu@student.manchester.ac.uk>
2026-06-03 20:38:57 +0100

    Initial Commit

---
+++ ChemVaultLabCompanion/ChemVaultLabCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemVaultLabCompanionApp.swift
+//  ChemVaultLabCompanion
+//
+//  Created by Edward on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChemVaultLabCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
